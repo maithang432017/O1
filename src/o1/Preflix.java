369a9c1acf6687bfa0899b9c68ce52d2d9981fb2
MY PC <MY PC@DESKTOP-GO2UTVP>
2023-05-02 18:46:03 +0700

    first commit

---
+++ src/o1/Preflix.java
@@ -0,0 +1,23 @@
+package o1;
+
+public class Preflix {
+	public static void main(String[] args) {
+		String[] array = { "flower", "flow", "flight" };
+		String getPreflix = getTheLongestPreflix(array);
+		System.out.println("the longest preflix is:" + getPreflix);
+	}
+
+	public static String getTheLongestPreflix(String[] arrs) {
+		if (arrs.length == 0) {
+			return "";
+		}
+		String preflix = arrs[0];
+		for (int i = 1; i < arrs.length; i++) {
+			while (arrs[i].indexOf(preflix) != 0) {
+				preflix = preflix.substring(0, preflix.length() - 1);
+			}
+		}
+		return preflix;
+	}
+
+}
